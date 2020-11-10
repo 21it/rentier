4438d1f2f04ae42a56070f71a4c6766f5aa359e6
tkachuk-labs <tkachuk.labs@gmail.com>
2020-11-10 23:52:33 +0200

    upgrade LTS

--- src/Handler/IframePropertySchedule.hs
+++ src/Handler/IframePropertySchedule.hs
@@ -1,35 +1,37 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE NamedFieldPuns        #-}
-{-# LANGUAGE NoImplicitPrelude     #-}
-{-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE QuasiQuotes           #-}
-{-# LANGUAGE TemplateHaskell       #-}
-{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Handler.IframePropertySchedule where
 
-import           Data.Time.Calendar
-import           Data.Time.Calendar.WeekDate
-import           Data.Time.Clock
-import           Data.Time.ISO8601
-import qualified Database.Persist            as P
-import           Import
-import           Rentier.FullCalendar
-import           Rentier.HumanReadable
-import           Rentier.Organization
-import           Rentier.Property
-import           Rentier.Time
-import           Rentier.WeekDay
+import Data.Time.Calendar
+import Data.Time.Calendar.WeekDate
+import Data.Time.Clock
+import Data.Time.ISO8601
+import qualified Database.Persist as P
+import Import
+import Rentier.FullCalendar
+import Rentier.HumanReadable
+import Rentier.Organization
+import Rentier.Property
+import Rentier.Time
+import Rentier.WeekDay
 
 getIframePropertyScheduleR :: PropertyId -> Handler TypedContent
 getIframePropertyScheduleR propertyId = do
-  po <- runDB $ select $
-        from $ \(p, o) -> do
-          where_ (p ^. PropertyId ==. val propertyId)
-          where_ (p ^. PropertyOrganizationId ==. o ^. OrganizationId)
-          return (p, o)
-
+  po <- runDB $ select
+    $ from
+    $ \(p, o) -> do
+      where_ (p ^. PropertyId ==. val propertyId)
+      where_ (p ^. PropertyOrganizationId ==. o ^. OrganizationId)
+      return (p, o)
   case po of
-    [(Entity{entityVal = Property{propertyKind = Primary}}, Entity{entityVal = org, entityKey = organizationId})] ->
+    [(Entity {entityVal = Property {propertyKind = Primary}}, Entity {entityVal = org, entityKey = organizationId})] ->
       selectRep $ do
         provideRep $ do
           --
@@ -50,22 +52,27 @@ getIframePropertyScheduleR propertyId = do
           --
           -- TODO : fix this unsafe code, remove "start" and "end" hardcoded parameters
           --
-          Just ds <- fmap (\x -> x >>= parseISO8601 . unpack >>= Just . utctDay) (lookupGetParam "start")
-          Just de <- fmap (\x -> x >>= parseISO8601 . unpack >>= Just . utctDay) (lookupGetParam "end")
+          ds <- liftMaybe =<< fmap (\x -> x >>= parseISO8601 . unpack >>= Just . utctDay) (lookupGetParam "start")
+          de <- liftMaybe =<< fmap (\x -> x >>= parseISO8601 . unpack >>= Just . utctDay) (lookupGetParam "end")
           scheduleList <- fmap (map entityVal) (runDB $ selectList [OrganizationScheduleOrganizationId P.==. organizationId] [])
-          let eventsList = [ds..de] >>= \d -> map (createEvent d) (schedule2Ranges (day2WeekDay d) scheduleList)
+          let eventsList = [ds .. de] >>= \d -> map (createEvent d) (schedule2Ranges (day2WeekDay d) scheduleList)
           return $ toEncoding eventsList
+      where
+        day2WeekDay :: Day -> WeekDay
+        day2WeekDay x =
+          toEnum $ intWeekDay - 1
           where
-            day2WeekDay :: Day -> WeekDay
-            day2WeekDay x =
-              toEnum $ intWeekDay - 1
-              where
-                (_, _, intWeekDay) = toWeekDate x
-            createEvent :: Day -> MinutesRange -> FullCalendarEvent
-            createEvent d (MinutesRange ms me) =
-              FullCalendarEvent{
-                fullCalendarEventStart = createUTCTime d ms,
-                fullCalendarEventEnd = createUTCTime d me
-              }
+            (_, _, intWeekDay) = toWeekDate x
+        createEvent :: Day -> MinutesRange -> FullCalendarEvent
+        createEvent d (MinutesRange ms me) =
+          FullCalendarEvent
+            { fullCalendarEventStart = createUTCTime d ms,
+              fullCalendarEventEnd = createUTCTime d me
+            }
     _ ->
       permissionDenied ""
+
+liftMaybe :: Maybe a -> Handler a
+liftMaybe = \case
+  Nothing -> permissionDenied ""
+  Just x -> return x
--- src/Import.hs
+++ src/Import.hs
@@ -1,11 +1,29 @@
 module Import
-    ( module Import
-    ) where
+  ( module Import,
+  )
+where
 
-import           Database.Esqueleto  as Import hiding (Value)
-import           Foundation          as Import
-import           Import.NoFoundation as Import hiding (count, delete, groupBy,
-                                                isNothing, on, selectSource,
-                                                update, (!=.), (*=.), (+=.),
-                                                (-=.), (/=.), (<.), (<=.), (=.),
-                                                (==.), (>.), (>=.), (||.))
+import Database.Esqueleto as Import hiding (Value)
+import Foundation as Import
+import Import.NoFoundation as Import hiding
+  ( (!=.),
+    (*=.),
+    (+=.),
+    (-=.),
+    (/=.),
+    (<&>),
+    (<.),
+    (<=.),
+    (=.),
+    (==.),
+    (>.),
+    (>=.),
+    count,
+    delete,
+    groupBy,
+    isNothing,
+    on,
+    selectSource,
+    update,
+    (||.),
+  )
