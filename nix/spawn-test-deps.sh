2f374e43bb39daa6b0712bff974663af9d6c45ce
tkachuk-labs <tkachuk.labs@gmail.com>
2021-08-01 16:27:11 +0300

    fix spawn-test-deps.sh

--- nix/spawn-test-deps.sh
+++ nix/spawn-test-deps.sh
@@ -19,29 +19,24 @@ else
   alias postgres-sh="su -m nixbld1"
 fi
 
-if [ ! -d $PGHOST ]; then
-  echo "initializing postgresql workspace..."
-  postgres-sh -c "mkdir -p $PGHOST"
-fi
-
-if [ ! -d $PGDATA ]; then
-  echo "initializing postgresql database..."
-  postgres-sh -c "mkdir -p $PGDATA"
-  postgres-sh -c "initdb $PGDATA --encoding=UTF8 --auth=trust > /dev/null"
-fi
+mkdir -p $PGHOST
+mkdir -p $PGDATA
 
 if [[ $EUID -ne 0 ]]; then
-  echo "operating as root..."
+  echo "spawn-test-deps.sh ===> operating as root..."
   chown -R root $PGDATA
   chown -R root $PGHOST
 else
-  echo "operating as nixbld1..."
+  echo "spawn-test-deps.sh ===> operating as nixbld1..."
   chown -R nixbld1 $PGDATA
   chown -R nixbld1 $PGHOST
 fi
 
-echo "starting postgres..."
-postgres-sh -c 'pg_ctl start -o "-c listen_addresses=localhost -c unix_socket_directories=$PGHOST" > /dev/null'
+echo "spawn-test-deps.sh ===> initializing postgresql..."
+postgres-sh -c "initdb $PGDATA --encoding=UTF8 --auth=trust >/dev/null"
+
+echo "spawn-test-deps.sh ===> starting postgres..."
+postgres-sh -c 'pg_ctl start -l $PGLOG -o "-c listen_addresses=localhost -c unix_socket_directories=$PGHOST" > /dev/null'
 
 until postgres-sh -c 'pg_isready' 2>/dev/null; do
   >&2 echo "postgres is unavailable - sleeping for 3 seconds..."
@@ -51,4 +46,5 @@ done
 postgres-sh -c 'createuser rentier'
 postgres-sh -c 'createdb rentier'
 postgres-sh -c 'createdb rentier_test'
-echo "spawn-test-deps executed"
+
+echo "spawn-test-deps.sh ===> executed"
